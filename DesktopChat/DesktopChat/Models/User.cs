dac3917bfe3c985afa3f8d913abba66b6270f1c4
ahmedehabb2 <ahmed.ehab.mansour@gmail.com>
2023-06-22 11:25:09 +0300

    desktop app creation

---
+++ DesktopChat/DesktopChat/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DesktopChat.Models
+{
+    internal class User
+    {
+        private int id;
+        private string name;
+        private string phone;
+        private string password;
+       
+        
+        
+    }
+}
